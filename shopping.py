1718617d8b5f734d74eddbc6875af6843caab328
idawatibustan <idawati.bustan@gmail.com>
2016-09-24 12:23:20 -0700

    Created shopping file

---
+++ shopping.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+class shopping:
+    def __init__(self, cps):
+        self.shoppes = []
+        self.items = []
+        self.checkretail(cps)
+        self.status = None
+
+    def hasretail(self):
+        if len(shoppes) == 0:
+            return False
+        return True
+
+    def checkretail(self, cps):
+        for cp in cps:
+            if cp.category == 'Retail':
+                self.shoppes.append(cp.name)
+
+    def loaditems(self):
+        for shop in self.shoppes:
+            items = shop.top_five()
+
+class purchases:
+    def __init__(self):
+        self.status = None
+        # processed, dispatched, delivered
+        self.items = []
+        self.totalprice = 0.00
+
+    def additem(self, item):
+        self.items.append(item)
+        self.totalprice += item.price
+
+    def checkout(self):
+        self.status = 'processed'
+        return self.totalprice
+
+    def isprocessed():
+        return self.status == 'processed'
+
+    def isdispatched():
+        return self.status == 'dispatched'
+
+    def isdelivered():
+        return self.status == 'delivered'
+
+class shop:
+    def __init__(self):
+        self.name = None
+        self.items = []
+    
+    def populate_items(self):
+        self.items = [{
+            Item('Chanel', 'Coco Au De Perfume', 72.00, 'img\\1.jpg'),
+            Item('DKNY', 'Fresh Blossom 100ml', 69.00, ''),
+            Item('Lancome' 'Miracle', 99.00, ''),
+            Item('Yves Saint Laurent','Black Opium EDP', 105.00, ''),
+            Item('Marc Jacobs', 'Daisy EDT', 93.00, ''),
+        }]
+
+    def top_five(self):
+        return self.items[0:4]
+
+class item:
+    def __init__(self, brand, name, price, image):
+        self.brand = None
+        self.name = None
+        self.price = None
+        self.image = None
+        self.status = 'standard'
+        #promo, exclusive, standard
