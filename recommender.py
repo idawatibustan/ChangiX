751fcd51224bd6b489482994a813e1f3b9c3858f
Andre Tan <tanandre93@gmail.com>
2016-09-24 14:40:20 -0700

    jesus fuck, it works!

--- recommender.py
+++ recommender.py
@@ -1,20 +1,19 @@
 from Queue import Queue
+from itertools import cycle
+from random import shuffle
+import json
 
 class Recommender:
-    def __init__(self, passenger):
+    def __init__(self, passenger, demo):
         self.passenger = passenger
-        checkpoints = generate_checkpoints()
-        checkpoints = [Checkpoint(c['name'], c['category'], c['subcategory']) for c in checkpoints]
-        self.q = self._sort(checkpoints)
+        self.is_demo = demo
         self.checkpoints_liked = []
         self.categories_disliked = []
+        checkpoints = generate_checkpoints(demo)
+        self.q = self._sort(checkpoints)
 
     def checkpoint_info(self):
-        '''
-        Call this method to get the next available checkpoint to be loaded into the card.
-        Returns None if no other checkpoints available.
-        '''
-        return None if self.q.empty() else self.q.get().as_dict()
+        return None if self.q.empty() else self.q.get()
 
     def like_checkpoint(self, checkpoint):
         self.checkpoints_liked.append(checkpoint)
@@ -23,60 +22,65 @@ class Recommender:
         category = checkpoint['category']
         self.categories_disliked.append(category)
 
+        # create new queue without the disliked category
+        new_q = Queue()
+        while not self.q.empty():
+            curr_checkpoint = self.q.get()
+            if curr_checkpoint['category'] not in self.categories_disliked:
+                new_q.put(curr_checkpoint)
+        self.q = new_q
+
     def recommendations(self):
         return self.checkpoints_liked
 
     def _sort(self, checkpoints):
         q = Queue()
-        [q.put(c) for c in checkpoints]
+        if self.is_demo:
+            [q.put(c) for c in checkpoints]
+        else:
+            # creates circular list of the unique categories
+            categories = cycle(set(c['category'] for c in checkpoints))
+
+            # format checkpoints into a dictionary where keys are the categories, and values are the checkpoints belonging to that category
+            chk_by_cat = {}
+            for checkpoint in checkpoints:
+                cat = checkpoint['category']
+                if cat in chk_by_cat:
+                    chk_by_cat[cat].append(checkpoint)
+                else:
+                    chk_by_cat[cat] = [checkpoint]
+
+            # create new queue where the order of elements cycle equally across the categories
+            while self._not_empty(chk_by_cat):
+                curr_cat = categories.next()
+                checkpoints_from_cat = chk_by_cat[curr_cat]
+                if checkpoints_from_cat:
+                    shuffle(checkpoints_from_cat)
+                    checkpoint = checkpoints_from_cat.pop()
+                else:
+                    checkpoint = None
+                chk_by_cat[curr_cat] = checkpoints_from_cat
+                if checkpoint:
+                    q.put(checkpoint)
         return q
 
+    def _not_empty(self, d):
+        return len([item for sublist in d.values() for item in sublist]) != 0
 
-class Checkpoint:
-    def __init__(self, name, category, subcategory):
-        self.name = name
-        self.category = category
-        self.subcategory = subcategory
-
-    def __str__(self):
-        return "%s (%s: %s)" % (self.name, self.category, self.subcategory)
-    def __unicode__(self):
-        return "%s (%s: %s)" % (self.name, self.category, self.subcategory)
-    def __repr__(self):
-        return "%s (%s: %s)" % (self.name, self.category, self.subcategory)
-
-    def as_dict(self):
-        return {
-            'name': self.name,
-            'category': self.category,
-            'subcategory': self.subcategory
-        }
-
-def generate_checkpoints():
-    return [
-        {
-            'name': 'Ajisen Ramen',
-            'category': 'Food',
-            'subcategory': 'Japanese',
-        }, {
-            'name': 'Indonesian Barbequeue',
-            'category': 'Food',
-            'subcategory': 'Indonesian'
-        }, {
-            'name': 'Giant Slide',
-            'category': 'Leisure',
-            'subcategory': 'Entertainment'
-        }, {
-            'name': 'Coco Chanel',
-            'category': 'Retail',
-            'subcategory': 'Cosmetics'
-        }, {
-            'name': 'Airport Wellness Oasis',
-            'category': 'Leisure',
-            'subcategory': 'Relaxation'
-        }
-    ]
+def generate_checkpoints(is_demo):
+    if is_demo:
+        checkpoints = json.loads(open("json/demo_checkpoints.json").read())
+    else:
+        checkpoints = json.loads(open("json/production_checkpoints.json").read())
+    return checkpoints
 
 if __name__=="__main__":
-    r = Recommender(None)
-    print r.checkpoint_info()
+    r = Recommender(None, demo=False)
+    r.dislike_checkpoint({'category':'Retail'})
+    boo = True
+    while boo:
+        cc = r.checkpoint_info()
+        if not cc:
+            boo = False
+        else:
+            print cc['name']
