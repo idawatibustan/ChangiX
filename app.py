5d539a849282a4126fe6d4c93112605f2bb612e6
Kyle Tan <kyle@kyletan.com>
2016-09-24 17:11:12 +0100

    Camera feature works

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import json
-from flask import Flask, render_template
+import base64
+import requests
+from flask import Flask, render_template, request, send_file, redirect, url_for
 from gevent.wsgi import WSGIServer
 from settings import *
 
@@ -9,6 +11,22 @@ app.debug = True
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/scan", methods=["GET", "POST"])
+def scan():
+    if request.method == "POST":
+        img = base64.b64decode(request.data)
+        open("file.png", "w").write(img)
+        files = {"media": open("file.png", "rb")}
+        r = requests.post("https://zxing.org/w/decode", files=files)
+        print r.text
+        return json.dumps("")
+    if request.method == "GET":
+        return send_file("file.png", mimetype="image/png")
+
+@app.route("/file/<file>")
+def reroute(file):
+    return send_file("file.png", mimetype="image/png")
 
 http_server = WSGIServer(("", PORT), app)
 http_server.serve_forever()
