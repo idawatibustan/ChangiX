e7ff7f9f791721fd1987e5678fcb6a9450c61fcc
idawatibustan <idawati.bustan@gmail.com>
2016-09-24 02:37:14 -0700

    created passenger file

---
+++ passenger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from datetime import datetime
+# import barcode_reader
+# import flight_reader
+
+class passenger:
+    def __init__(self, id):
+        b = barcode_reader(id)
+
+        self.name = b.get('firstname', "Jane") + " " + b.get('lastname', "Doe")
+        self.origin = b.get('origin', "AAA")
+        self.dest = b.get('destination', "ZZZ")
+        self.flightnum = b.get('airlines', "XX")
+        self.date = b.get('date')
+        self.seat = b.get('seat')
+        self.seq = b.get('seq')
+
+        self.starttime = datetime.now()
+        self.startloc = None
+        self.currloc = None
+        self.boardtime = None
+        self.depttime = None
+        self.boardgate = None
+
+        self.updateflight()
+
+    def updateflight(self):
+        f = flight_reader(self.flightnum)
+        self.boardtime = f.boardingtime
+        self.depttime = f.departtime
+        self.boardgate = flight.gate
